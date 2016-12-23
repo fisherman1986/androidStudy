72d3556243a1380602e9c4fb19de9a60eb2181c3
zhj <zuohongjie@126.com>
2016-12-23 10:21:37 +0800

    111

---
+++ app/src/main/java/com/xdtdz/study/androidstudy/WelComeActivity.java
@@ -0,0 +1,26 @@
+package com.xdtdz.study.androidstudy;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Handler handler = new Handler();
+        //当计时结束,跳转至主界面
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, FirstActivity.class);
+                startActivity(intent);
+                WelcomeActivity.this.finish();
+            }
+        }, 2000);
+
+    }
+}
