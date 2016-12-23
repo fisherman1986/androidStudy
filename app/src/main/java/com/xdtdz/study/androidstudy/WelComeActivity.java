a6a70db358c6de37ef1f1eed34d0a86ec787bb55
zhj <zuohongjie@126.com>
2016-12-23 14:04:02 +0800

    111

--- app/src/main/java/com/xdtdz/study/androidstudy/WelComeActivity.java
+++ app/src/main/java/com/xdtdz/study/androidstudy/WelComeActivity.java
@@ -1,16 +1,20 @@
 package com.xdtdz.study.androidstudy;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE) //exends Activity;
         setContentView(R.layout.activity_welcome);
+        getSupportActionBar().hide();  //extends AppCompatActivity
         Handler handler = new Handler();
         //当计时结束,跳转至主界面
         handler.postDelayed(new Runnable() {
